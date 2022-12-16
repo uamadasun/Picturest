23325da392446e573ab1b13178a4827b823cce9d
uamadasun <uchennaamadasun@gmail.com>
2022-12-16 09:41:57 -0800

    created picturest project in spring

---
+++ Picturest/src/main/java/com/codingdojo/picturest/PicturestApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.picturest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicturestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicturestApplication.class, args);
+	}
+
+}
---
+++ Picturest/src/test/java/com/codingdojo/picturest/PicturestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.picturest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicturestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
